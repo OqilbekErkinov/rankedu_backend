28f6371b837e747d6210b712bc142c4dfc4db247
Oqilbek <erkinovoqilbek@gmail.com>
2025-11-26 16:40:31 +0500

    update

--- rankedu_backend/settings.py
+++ rankedu_backend/settings.py
@@ -29,21 +29,10 @@ SECRET_KEY = 'django-insecure-$7_5oflwiq#ky4y)$bb%ihrh3owc9m#9k=td+7-9h)3_h02cvc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "0.0.0.0",
-    "rankedu-backend-wsmj-g.fly.dev",
-    "rank-edu.vercel.app",   # frontend domening
-]
 
 # ALLOWED_HOSTS = os.getenv("127.0.0.1", "localhost").split(",")
 CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = [
-    "https://rankedu-backend-wsmj-g.fly.dev",
-    "https://rank-edu.vercel.app",
-    "https://*.fly.dev",
-]
+
 
 # Application definition
 
@@ -160,3 +149,20 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# === Fly.io override: admin & API uchun aniq hostlar ===
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+    "rankedu-backend-wsmj-g.fly.dev",  # backend domen
+    "rank-edu.vercel.app",             # frontend domen
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://rankedu-backend-wsmj-g.fly.dev",
+    "https://rank-edu.vercel.app",
+    "https://*.fly.dev",
+]
+
